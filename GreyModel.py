c33f8e8bd6a3cf37ec0a6d99aba07ee76dd5ec79
mikelei8291 <mikelei8291@gmail.com>
2016-03-27 20:12:14 +0800

    Question3 and more

---
+++ GreyModel.py
@@ -0,0 +1,8 @@
+rawData = [a,b]
+n = len(rawData))
+print(n)
+lmbda = rawData*()
+dataRange = max(rawData) - min(rawData)
+for i in range(n+1):
+    fstAdd = rawData.append(rawData[0] + rawData[1])
+print(fstAdd)
---
+++ d.m
@@ -0,0 +1,3 @@
+syms x y;
+k=
+y=dsolve('Dy-n*y+k+(z+w)','x')
