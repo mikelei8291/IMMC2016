398bcd75b5508ef2207b48639c3618921bfc8d92
mikelei8291 <mikelei8291@gmail.com>
2016-03-29 02:56:35 +0800

    Q3/4/5 and some sort out works

--- GreyModel.py
+++
@@ -1,8 +0,0 @@
-rawData = [a,b]
-n = len(rawData))
-print(n)
-lmbda = rawData*()
-dataRange = max(rawData) - min(rawData)
-for i in range(n+1):
-    fstAdd = rawData.append(rawData[0] + rawData[1])
-print(fstAdd)
--- d.m
+++
@@ -1,3 +0,0 @@
-syms x y;
-k=
-y=dsolve('Dy-n*y+k+(z+w)','x')
